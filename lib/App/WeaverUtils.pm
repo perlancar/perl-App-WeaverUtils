4b6dd870a9affd94c1fc98c9b372120a9c9e45e7
perlancar (@pc-home) <perlancar@gmail.com>
2015-03-21 13:57:17 +0700

    [Documentation] Mention list of utilities in main module, add FAQ & more complete See Also

--- lib/App/WeaverUtils.pm
+++ lib/App/WeaverUtils.pm
@@ -10,7 +10,18 @@ use 5.010001;
 
 =head1 DESCRIPTION
 
-This distribution provides several command-line utilities related to
-L<Pod::Weaver>.
+This distribution provides the following command-line utilities related to
+L<Pod::Weaver>:
+
+#INSERT_EXECS_LIST
+
+=head1 FAQ
+
+#INSERT_BLOCK: App::PMUtils faq
+
+
+=head1 SEE ALSO
+
+#INSERT_BLOCK: App::PMUtils see_also
 
 =cut
